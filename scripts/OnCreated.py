c5963a3beffe8c93bb23cb4ad2f39ad55bfddf20
Lucas Galante <lsgalante12@gmail.com>
2026-03-25 10:08:04 -0400

    Restore default node shape

--- scripts/OnCreated.py
+++ scripts/OnCreated.py
@@ -8,6 +8,6 @@ node = Node(kwargs['node'])
 
 # Force the shape to your desired JSON name
 # (e.g., "rect", "circle", or your custom "my_shape")
-# node.setUserData("nodeshape", "squared")
+node.setUserData("nodeshape", "rect")
 node.setColor(hou.Color(Prefs().node_color))
 # snapToGrid(node)
